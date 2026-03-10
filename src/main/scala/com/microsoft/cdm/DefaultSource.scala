c6cee537f6d1b8770713789661fddc4fcdf4d6d4
Erik Andreas Røkland <erik.rokland@bouvet.no>
2026-03-10 14:46:00 +0100

    add support for unity catalog

--- src/main/scala/com/microsoft/cdm/DefaultSource.scala
+++ src/main/scala/com/microsoft/cdm/DefaultSource.scala
@@ -1,48 +1,29 @@
 package com.microsoft.cdm
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.connector.catalog.{Identifier, SupportsCatalogOptions, Table}
+import org.apache.spark.sql.connector.catalog.{Table, TableProvider}
 import org.apache.spark.sql.connector.expressions.Transform
+import org.apache.spark.sql.sources.DataSourceRegister
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 import org.slf4j.LoggerFactory
+import com.microsoft.cdm.utils.CDMOptions
 
 
-class DefaultSource extends SupportsCatalogOptions{
+class DefaultSource extends TableProvider with DataSourceRegister {
 
     val logger  = LoggerFactory.getLogger(classOf[DefaultSource])
 
     override def inferSchema(options: CaseInsensitiveStringMap): StructType = {
-       null
+        new HadoopTables().load(new CDMOptions(options)).schema
     }
     
     override def getTable(structType: StructType, transforms: Array[Transform], map: java.util.Map[String, String]): Table = {
-        try{
         val caseInsensitiveStringMap = new CaseInsensitiveStringMap(map)
-        val schema = if (structType != null) {
-            structType
-        } else {
-            inferSchema(caseInsensitiveStringMap)
-        }
+        val schema = if (structType != null) structType else inferSchema(caseInsensitiveStringMap)
         new SparkTable(schema, caseInsensitiveStringMap)
-        } catch {
-            case _ : Exception => {
-                 null
-            }
-        }
     }
 
     override def supportsExternalMetadata(): Boolean = true
 
-    override def extractIdentifier(options: CaseInsensitiveStringMap): Identifier = {
-        val spark = SparkSession.active;
-        spark.conf.set("spark.sql.catalog.cdm", "com.microsoft.cdm.CDMCatalog")
-        val cdmcatalog = spark.sessionState.catalogManager.catalog("cdm")
-        cdmcatalog.asInstanceOf[CDMCatalog].setupOptions(options)
-        new CDMIdentifier(options)
-    }
-
-    override def extractCatalog(options: CaseInsensitiveStringMap): String = {
-        "cdm"
-    }
+    override def shortName(): String = "cdm"
 }
